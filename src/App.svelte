3e520c93106ec77969f0257532693f767970dd9c
Ricardo Tomasi <ricardobeat@gmail.com>
2025-12-08 00:15:31 +0100

    re-enable persistent ID

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script>
   import Header from "src/components/Header.svelte";
   import { Router } from "sv-router";
+  import { onMount } from "svelte";
+  import { setCurrentUserId } from "src/stores/user";
   import "./router.ts";
+
+  onMount(() => {
+    window.electron.onInitialData((data) => {
+      setCurrentUserId(data.token);
+    });
+  });
 </script>
 
 <vstack class="main">
