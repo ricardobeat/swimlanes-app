f7486dac874193ca92f0ff7f0d55da11eedca5ab
Ricardo Tomasi <ricardobeat@gmail.com>
2025-12-08 00:15:31 +0100

    add error page

---
+++ src/pages/Ooops.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { navigate } from "src/router";
+  import { applicationError } from "src/stores/error";
+
+  const restart = (): void => {
+    navigate("/");
+  };
+</script>
+
+<vstack h-center v-center flex gap="3">
+  <h2>Ooops... something went wrong.</h2>
+  <p><a href="##" onclick={restart}>try again?</a></p>
+  <pre>{$applicationError}</pre>
+</vstack>
+
+<style>
+  h2 {
+    font-family: var(--font-header);
+    font-weight: 600;
+    font-size: var(--font-size-subtitle);
+  }
+  p {
+    font-size: 0.8rem;
+  }
+  pre {
+    margin-top: 10%;
+    font-size: 10px;
+  }
+</style>
