03f93940d1cc0736481e756b3cc7aa753cf7f373
Ricardo Tomasi <ricardobeat@gmail.com>
2025-12-08 00:15:31 +0100

    use ky http library

---
+++ src/api/api.ts
@@ -0,0 +1,56 @@
+import ky from "ky";
+import type { Task } from "../types/task";
+import type { Board } from "../types/board";
+import { getUserId } from "../stores/user";
+
+const API_URL = "http://localhost:3002";
+
+const request = ky.create({
+  prefixUrl: API_URL,
+  hooks: {
+    beforeRequest: [(request) => request.headers.set("X-User-ID", getUserId())],
+  },
+});
+
+export interface ServerEvent {
+  boardId: string;
+  task: Task;
+  action: "create" | "update" | "delete";
+}
+
+class ServiceAPI {
+  async createBoard(user: string): Promise<Board> {
+    return request.post<Board>("/boards", { json: { user } }).json();
+  }
+
+  async getBoard(boardId: string): Promise<Task[]> {
+    return request.get<Board>(`/boards/${boardId}`).json();
+  }
+
+  async createTask(boardId: string, task: Task): Promise<Task> {
+    return request.post(`/boards/${boardId}/tasks`, { json: task }).json();
+  }
+
+  async updateTask(boardId: string, task: Task): Promise<Task> {
+    return request.put(`/boards/${boardId}/tasks/${task.id}`, { json: task }).json();
+  }
+
+  async deleteTask(boardId: string, taskId: string): Promise<void> {
+    return request.delete(`/boards/${boardId}/tasks/${taskId}`).json();
+  }
+
+  subscribe(boardId: string, onEvent: (event: ServerEvent) => void): EventSource {
+    const evtSource = new EventSource(`${API_URL}/events?stream=${boardId}`);
+    evtSource.onmessage = (e) => {
+      try {
+        const data = JSON.parse(e.data);
+        onEvent(data);
+      } catch (err) {
+        console.error("Failed to parse SSE event", err);
+      }
+    };
+    return evtSource;
+  }
+}
+
+export const api = new ServiceAPI();
